317c9ac621b814521dbc266dcef890a54e74a9ac
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-10-30 01:52:15 +0900

    修正しました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -89,10 +89,20 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
+      it 'priceが9_999_999円以上だと登録できない' do
+        @item.price = '10_000_000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
       it 'priceは半角数値以外では登録できない' do
         @item.price = '12a'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
