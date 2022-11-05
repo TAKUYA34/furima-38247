50e9ed4f38122ccdff5c5760e0f97a5ded27e5a1
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-11-05 19:10:46 +0900

    Revert "destroyアクションを定義"
    This reverts commit 5553bd184254f66a9df366f99b026748cfd9fcd2.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -33,12 +33,6 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
-
 
   private
   def item_params
