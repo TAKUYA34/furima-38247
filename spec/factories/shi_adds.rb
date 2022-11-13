4fbc5c50941dd8f56ba9be03114eec0d15c785ff
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-11-14 00:13:36 +0900

    テストコードで不要になったファイル等

---
+++ spec/factories/shi_adds.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :shi_add do
+    # post_code         {222-3444}
+    # sender_id         {2}
+    # locality          {松前市}
+    # address           {青山1-2-2}
+    # phone             {09088554321}
+    # association :order
+  end
+end
